cc73e2e02f765eed086ef99e57abeb2937abd85a
Arthur Alexandre <arthur_dda@hotmail.com>
2021-09-07 18:18:31 -0300

    Armazenando as tasks e atualizando a lista dinamicamente

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +7,22 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tasks: any[] = [];
 
-  constructor(private alertCtrl: AlertController) {}
+  constructor(private alertCtrl: AlertController,private toastCtrl: ToastController) {
+      const taskJson = localStorage.getItem('LocalDB');
+      if(taskJson!=null){
+        this.tasks = JSON.parse(taskJson);
+      }
+    }
+
 
   async addTask(){
     const alert = await this.alertCtrl.create({
       header: 'O que deseja fazer?',
       inputs:[
         {
-          name: 'task',
+          name: 'newTask',
           type: 'text',
           placeholder: 'Digite o nome da tarefa aqui'
         }
@@ -32,12 +39,30 @@ export class HomePage {
         {
           text: 'Adicionar',
           handler: (form)=>{
-           // this.add(form.task);
+            this.add(form.newTask);
           }
         }
       ]
     });
     alert.present();
+  }
+  async add(newTask: string){
+    if(newTask.trim().length<1){
+      const toast = await this.toastCtrl.create({
+        message: 'O que deseja fazer?',
+        duration: 2000,
+        position: 'top'
+      });
+      toast.present();
+      return;
+    }
+    const task = {name: newTask, done: false};
+    this.tasks.push(task);
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage(){
+    localStorage.setItem('LocalDB', JSON.stringify(this.tasks));
   }
   editTask(){
     alert('editou');
